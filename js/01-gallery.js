17c00fc12f45f29f485c55879bf16c4172265aa4
Никита Шевченко <62301874+BornBeBat@users.noreply.github.com>
2023-11-19 16:17:46 +0200

    Task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const marcupConteiner = document.querySelector(".gallery");
+const cardsMarcup = createMarcup(galleryItems);
 
-console.log(galleryItems);
+marcupConteiner.insertAdjacentHTML("beforeend", cardsMarcup);
+
+marcupConteiner.addEventListener("click", onClick);
+
+function onClick(event) {
+  const { target = event.target } = event;
+  event.preventDefault();
+  if (!target.classList.contains("gallery__image")) {
+    return;
+  }
+  const lightBox = basicLightbox.create(
+    `<img src="${target.dataset.source}" width="800" height="600">`
+  );
+  lightBox.show();
+}
+
+function createMarcup(images) {
+  const marcup = images.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+    </a>
+    </li>`;
+  });
+  return marcup.join("");
+}
